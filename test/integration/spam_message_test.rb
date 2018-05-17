039b44e3233ead47270cf030e135f6287f24cb1e
Bèr Kessels <ber@berk.es>
2018-05-17 10:22:13 +0200

    Check params for spam flags, and return blank when found

---
+++ test/integration/spam_message_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+describe 'attempt to spam message through REST' do
+  let(:params) do
+    {
+      name: 'Harry Potter',
+      email: 'harry@hogwards.edu.wizard',
+      message: 'Wingardium Leviosar',
+      contact_captcha: 'true'
+    }
+  end
+
+  describe 'POST /messages' do
+    it 'does not send the message over SMTP but fakes the response' do
+      assert_no_changes 'Mail::TestMailer.deliveries.length' do
+        post '/messages', params
+        assert_response 201
+      end
+    end
+  end
+end
